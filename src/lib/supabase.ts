2951f31914922e7f351b17db46ed0aa14986ca82
Prit Ahir <prit@poly.dev>
2026-06-25 01:17:38 +0800

    fix: correct supabase project ref

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL! || "https://idyspefcwqpdnpvqrjtv.supabase.co";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL! || "https://iekpnqoiohmfmzqbauyh.supabase.co";
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY! || 
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlkeXNwZWZjd3FwZG5wdnFyanR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyMjM4MzIsImV4cCI6MjA5Nzc5OTgzMn0.Xwy6VjOUcK87Dl4QYobGat_a6ayQ-D9ndDNQrkQgNa8";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlla3BucW9pb2htZm16cWJhdXloIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIxNzQxMDEsImV4cCI6MjA5Nzc1MDEwMX0.S5CIfRR0BrhdbLG2UDDwDbu8o13hwfWWX3QZUE4C4t0";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
